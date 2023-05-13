18a8fe9dda2dff67c8dc1b40668fe5e4b47f79d9
Alex Racapé <alex.racape@gmail.com>
2023-05-12 20:33:09 -0400

    Outline plan in ReadMe

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Using machine learning and principles from finance to trade lego sets
 
 ## The Data
-The data in this project uses a combination of data from multiple sources
-- Features and list price from 1970 - 2015 from seankross on github
+This project uses a combination of data from multiple sources
+- Features and list price from 1970 - 2015 from [seankross](https://github.com/seankross/lego) on github
 - Current Price - Bricklink API as of 05/10/2023
 - Recent list prices and features - Brickset API as of 05/10/2023
 
@@ -19,3 +19,13 @@ TBD
 - Construct benchmark to compare results with
 - Can we spin as fundamental analysis?
 - Need more finance content...
+- Could we get number of sets out there and construct market cap?
+
+## Plan Going Forward
+- Figure out best hyperparameters and tune models to predict set value
+- See if we can find number of sets manufactured
+- Use model to identify undervalued sets
+- Buy undervalued sets and see how the cohort does, maybe go by year?
+  - Ex: train on all except 1995, then buy sets from that year
+- Compare returns to benchmark and S&P 500 for kicks
+- Can also experiment with forecasting into the future with timedelta as a parameter
--- src/random_forest.py
+++ src/random_forest.py
@@ -2,8 +2,9 @@
 from sklearn.model_selection import KFold
 from sklearn.metrics import mean_squared_error
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from BootstrapLearner import BootstrapLearner
 from PERTLearner import PERTLearner
@@ -22,54 +23,71 @@ def main():
     clean = data[["Year", "Pieces", "Minifigures", "Theme", "USD_MSRP", "Current_Price"]]
     data = clean.to_numpy()
 
-    # Set up learner
-    learner = BootstrapLearner(constituent=PERTLearner, kwargs={}, bags=20)
+    # Run experiments to tune hyperparameters
+    bag_vals = range(1, 50, 5)
+    experiment_storage = np.zeros((len(bag_vals), 5))
+    for num_bags in bag_vals:
+        print("Running experiment with {} bags".format(num_bags))
 
-    # Set up cross validation
-    num_folds = 10
-    kf = KFold(n_splits=num_folds, shuffle=True)
+        # Set up learner
+        learner = BootstrapLearner(constituent=PERTLearner, kwargs={}, bags=20)
 
-    # Train and evaluate for each fold
-    rmse_scores = []
-    correlation_scores = []
-    rmse_in_sample_scores = []
-    correlation_in_sample_scores = []
-    for train_indices, test_indices in kf.split(data):
+        # Set up cross validation
+        num_folds = 5
+        kf = KFold(n_splits=num_folds, shuffle=True)
 
-        # Get split
-        x_train, y_train = data[train_indices, :-1], data[train_indices, -1]
-        x_test, y_test = data[test_indices, :-1], data[test_indices, -1]
+        # Train and evaluate for each fold
+        rmse_scores = []
+        correlation_scores = []
+        rmse_in_sample_scores = []
+        correlation_in_sample_scores = []
+        for train_indices, test_indices in kf.split(data):
 
-        # Train and fit
-        learner.train(x_train, y_train)
+            # Get split
+            x_train, y_train = data[train_indices, :-1], data[train_indices, -1]
+            x_test, y_test = data[test_indices, :-1], data[test_indices, -1]
 
-        # Test in sample
-        in_sample_predictions = learner.test(x_train)
-        rmse_in_sample = mean_squared_error(y_train, in_sample_predictions, squared=False)
-        correlation_in_sample = np.corrcoef(y_train.astype(np.float64), in_sample_predictions)[0, 1]
-        rmse_in_sample_scores.append(rmse_in_sample)
-        correlation_in_sample_scores.append(correlation_in_sample)
+            # Train and fit
+            learner.train(x_train, y_train)
 
-        # Test out of sample
-        predictions = learner.test(x_test)
-        rmse = mean_squared_error(y_test, predictions, squared=False)
-        correlation = np.corrcoef(y_test.astype(np.float64), predictions)[0, 1]
-        rmse_scores.append(rmse)
-        correlation_scores.append(correlation)
+            # Test in sample
+            in_sample_predictions = learner.test(x_train)
+            rmse_in_sample = mean_squared_error(y_train, in_sample_predictions, squared=False)
+            correlation_in_sample = np.corrcoef(y_train.astype(np.float64), in_sample_predictions)[0, 1]
+            rmse_in_sample_scores.append(rmse_in_sample)
+            correlation_in_sample_scores.append(correlation_in_sample)
 
-    # Compute the mean scores over all folds
-    mean_rmse_os = np.mean(rmse_scores)
-    mean_correlation_os = np.mean(correlation_scores)
-    mean_rmse_is = np.mean(rmse_in_sample_scores)
-    mean_correlation_is = np.mean(correlation_in_sample_scores)
+            # Test out of sample
+            predictions = learner.test(x_test)
+            rmse = mean_squared_error(y_test, predictions, squared=False)
+            correlation = np.corrcoef(y_test.astype(np.float64), predictions)[0, 1]
+            rmse_scores.append(rmse)
+            correlation_scores.append(correlation)
 
-    # Print Results
-    print(f"Mean Squared Error (in sample): {mean_rmse_is}\n\t{rmse_in_sample_scores}")
-    print(f"Correlation (in sample): {mean_correlation_is}\n\t{correlation_in_sample_scores}")
-    print(f"Mean Squared Error: {mean_rmse_os}\n\t{rmse_scores}")
-    print(f"Correlation: {mean_correlation_os}\n\t{correlation_scores}")
-    # First try: is_corr = .96, os_corr = .65 so way better than neural net for now
-    # Need to run tests while varying bags and constituents to see how it changes
+        # Compute the mean scores over all folds
+        mean_rmse_os = np.mean(rmse_scores)
+        mean_correlation_os = np.mean(correlation_scores)
+        mean_rmse_is = np.mean(rmse_in_sample_scores)
+        mean_correlation_is = np.mean(correlation_in_sample_scores)
+
+        # Print Results
+        print(f"Mean Squared Error (in sample): {mean_rmse_is}\n\t{rmse_in_sample_scores}")
+        print(f"Correlation (in sample): {mean_correlation_is}\n\t{correlation_in_sample_scores}")
+        print(f"Mean Squared Error: {mean_rmse_os}\n\t{rmse_scores}")
+        print(f"Correlation: {mean_correlation_os}\n\t{correlation_scores}")
+        # First try: is_corr = .96, os_corr = .65 so way better than neural net for now
+
+        # Store results
+        experiment_storage[num_bags // 5 - 1, 0] = num_bags
+        experiment_storage[num_bags // 5 - 1, 1] = mean_rmse_is
+        experiment_storage[num_bags // 5 - 1, 2] = mean_correlation_is
+        experiment_storage[num_bags // 5 - 1, 3] = mean_rmse_os
+        experiment_storage[num_bags // 5 - 1, 4] = mean_correlation_os
+
+    # Plot experiment results
+    exp_data = pd.DataFrame(experiment_storage, columns=["Bags", "IS RMSE", "IS Correlation", "OS RMSE", "OS Correlation"])
+    exp_data.plot(x="Bags", y=["IS RMSE", "OS RMSE"])
+    plt.show()
 
 
 if __name__ == "__main__":
